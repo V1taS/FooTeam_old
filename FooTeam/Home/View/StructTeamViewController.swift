a0b0b3ba84fae5a5429c31da469697fc6b5d89ff
Vitalii Sosin <375693@mail.ru>
2020-05-25 04:02:03 +0300

    Create model MVC

---
+++ FooTeam/Home/View/StructTeamViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StructurePlayers.swift
+//  FooTeam
+//
+//  Created by Виталий Сосин on 14.05.2020.
+//  Copyright © 2020 Vitalii Sosin. All rights reserved.
+//
+import UIKit
+
+class StructTeamViewController: UIViewController {
+    
+    @IBOutlet var photoTeamOne: [UIImageView]!
+    @IBOutlet var nameTeamOne: [UILabel]!
+    
+    @IBOutlet var photoTeamTwo: [UIImageView]!
+    @IBOutlet var nameTeamTwo: [UILabel]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ModelAddPlayer.addPlayerNameandImage(nameTeamOne: &self.nameTeamOne,
+                                 photoTeamOne: &self.photoTeamOne,
+                                 nameTeamTwo: &self.nameTeamTwo,
+                                 photoTeamTwo: &self.photoTeamTwo)
+    }
+}
